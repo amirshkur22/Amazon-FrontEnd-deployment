9589ac685555f6cee1cc0182fb5b61473abd666a
amirshkur22 <amirshkurhu@gmail.com>
2024-05-28 20:37:27 +0300

    baseURL update

--- src/Api/Axios.jsx
+++ src/Api/Axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axoiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-8db4d/us-central1/api",
-  baseURL: "https://amazon-backend-deployment-tfh9.onrender.com",
+  // baseURL: "https://amazon-backend-deployment-tfh9.onrender.com",
+  baseURL: "http://localhost:2024",
 });
 export default axoiosInstance;
